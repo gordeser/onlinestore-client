d0865ff26e0da9d85f4a4b65a831ee208f009222
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-03 14:35:27 +0100

    Add auth to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import Menu from "./Menu";
 import {Link} from "react-router-dom";
+import Cookies from "js-cookie";
 
 
-const Header = () => {
+const Header = ({isAuthed, onLogout}) => {
     return (
         <div className="container">
             <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
@@ -14,15 +15,22 @@ const Header = () => {
 
                 <Menu />
 
-                <div className="col-md-3 text-end">
-                    <Link to="login">
-                        <button type="button" className="btn btn-outline-primary me-2">Login</button>
-                    </Link>
+                {isAuthed ? (
+                    <div className="col-md-4 text-end">
+                        <span>{'Logged in as ' + Cookies.get("email")}</span>
+                        <button type="button" className="btn btn-primary ms-4" onClick={onLogout}>Log out</button>
+                    </div>
+                ) : (
+                    <div className="col-md-3 text-end">
+                        <Link to="login">
+                            <button type="button" className="btn btn-outline-primary me-2">Login</button>
+                        </Link>
 
-                    <Link to="signup">
-                        <button type="button" className="btn btn-primary">Sign up</button>
-                    </Link>
-                </div>
+                        <Link to="signup">
+                            <button type="button" className="btn btn-primary">Sign up</button>
+                        </Link>
+                    </div>
+                )}
             </header>
         </div>
     )
