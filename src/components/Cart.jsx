03eb745dcd0d5ea9748e61ec5730456f988d813a
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-03 23:42:41 +0100

    products are now showing in Cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,27 +1,86 @@
-import React from "react";
+import {useEffect, useState} from "react";
+import Cookies from "js-cookie";
 
-const Cart = () => {
+const Cart = ({ isAuthed, price }) => {
+    const [productsCount, setProductsCount] = useState({});
+
+    useEffect(() => {
+        const cartCookie = Cookies.get('cart');
+        if (cartCookie) {
+            const productIds = cartCookie.split(':').map(Number);
+            const count = productIds.reduce((acc, id) => {
+                acc[id] = (acc[id] || 0) + 1;
+                return acc;
+            }, {});
+            setProductsCount(count);
+        }
+    }, []);
+
+
+    const [productsInfo, setProductsInfo] = useState({});
+
+    useEffect(() => {
+        async function fetchProductInfo(id) {
+            const response = await fetch(`http://localhost:8080/api/products/${id}`);
+            return await response.json();
+        }
+
+        Object.keys(productsCount).forEach(async (id) => {
+            const info = await fetchProductInfo(id);
+            setProductsInfo(prev => ({...prev, [id]: info}));
+        });
+
+    }, [productsCount]);
+
+    // todo
+    const handleOrder = () => {
+
+    }
+
     return (
         <div className="container py-4 align-content-center">
             <h1 className="mb-5 text-center fw-normal">Cart</h1>
+            {Object.keys(productsInfo).map(id =>
+                <div key={id} className="justify-content-center mb-5 row align-items-center my-3">
+                    <div className="col-1">
+                        <img src={productsInfo[id].image} className="img-fluid" alt={productsInfo[id].name} style={{maxWidth: "100px"}} />
+                    </div>
+                    <div className="col-1"><strong>{productsInfo[id].name}</strong></div>
 
-            <div className="justify-content-center mb-5 row align-items-center my-3">
-                <div className="col-1">
-                    <img src="" className="img-fluid" alt="Item" style={{"max-width": "100px;"}} />
+                    <div className="ms-5 col-1">{productsCount[id]} items</div>
+                    <div className="col-1">*</div>
+                    <div className="col-1">{Math.round(productsInfo[id].price * 100) / 100} CZK</div>
+                    <div className="col-1">=</div>
+                    <div className="col-1"><strong>{Math.round(productsCount[id] * productsInfo[id].price * 100) / 100} CZK</strong></div>
                 </div>
-                <div className="col-2"><strong>Item name</strong></div>
+            )}
 
-                <div className="col-auto">1x</div>
-                <div className="col-auto">*</div>
-                <div className="col-auto">5</div>
-                <div className="col-auto">=</div>
-                <div className="col-auto">100 CZK</div>
-            </div>
+            {Object.keys(productsInfo).length === 0 && (
+                <div>
+                    <h3 className="mb-5 text-center fw-normal">There are no items in your cart</h3>
+                </div>
+            )}
 
-            <div className="d-flex justify-content-center">
-                <button className="btn btn-primary">Make an order</button>
-            </div>
-
+            {Object.keys(productsInfo).length !== 0 && (
+                <>
+                    <div className="d-flex justify-content-center">
+                        <h4>Total: {price} CZK</h4>
+                    </div>
+                    {isAuthed ? (
+                        <div className="d-flex justify-content-center">
+                            <button className="btn btn-primary" onClick={handleOrder}>Make an order</button>
+                        </div>) : (
+                        <div className="d-flex justify-content-center alert alert-warning" style={{width: "30%", margin: "auto"}} role="alert">
+                            <span className="me-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+                                        <path d="M8.982 1.318a.5.5 0 00-.964 0L1 13h14L8.982 1.318zM8 5a.5.5 0 011 0v3a.5.5 0 010 1H8V5zM8 12a.5.5 0 110-1 .5.5 0 010 1z"/>
+                                    </svg>
+                                </span>
+                            Only signed up users can make orders.
+                        </div>
+                    )}
+                </>
+            )}
         </div>
     )
 }
