c7fb1cd196c0e3536f8d483b54718285a30300f2
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-03 14:32:55 +0100

    add authentication to app component

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,36 @@ import Cart from "./components/Cart";
 import MainLayout from "./layouts/MainLayout";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import {useEffect, useState} from "react";
+import Cookies from "js-cookie";
 import ProductPage from "./components/ProductPage";
 
 function App() {
+    const [isAuthed, setIsAuthed] = useState(!!Cookies.get("email"));
+
+    const handleLogin = () => {
+        setIsAuthed(true);
+    }
+
+    const handleLogOut = () => {
+        setIsAuthed(false);
+        alert("Successfully logged out");
+        Cookies.remove("email");
+    }
+
+    const handleSignup = () => {
+        setIsAuthed(true);
+    }
+
     return <BrowserRouter>
         <div>
             <Routes>
-                <Route path="/" element={<MainLayout />}>
                     <Route path="products" element={<Products />} />
+                <Route path="/" element={<MainLayout isAuthed={isAuthed} onLogout={handleLogOut} />}>
                     <Route path="/products/:id" element={<ProductPage />} />
                     <Route path="cart" element={<Cart />} />
-                    <Route path="login" element={<Login />} />
-                    <Route path="signup" element={<Signup />} />
+                    <Route path="login" element={<Login onLogin={handleLogin} />} />
+                    <Route path="signup" element={<Signup onSignup={handleSignup}/>} />
 
                     <Route path="*" element={<NotFound />} />
                 </Route>
