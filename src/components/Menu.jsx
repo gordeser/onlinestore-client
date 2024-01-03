27976aa684090bf56e2ef49ff241602dd38ce5fe
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-03 14:38:15 +0100

    add total price to Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,11 @@
 import { NavLink } from "react-router-dom";
 
-
-const Menu = () => {
+const Menu = ({price}) => {
     return (
         <nav className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
             <NavLink to="." className="nav-link px-2">Home</NavLink>
             <NavLink to="products" className="nav-link px-2">Products</NavLink>
-            <NavLink to="cart" className="nav-link px-2">Cart: 0 CZK</NavLink>
+            <NavLink to="cart" className="nav-link px-2">Cart: {price} CZK</NavLink>
         </nav>
     )
 }
