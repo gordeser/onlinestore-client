89dc23b4b239dc025346a5b01aec7e9cd43a6a4a
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-03 14:37:49 +0100

    add total price to MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 
-const MainLayout = ({isAuthed, onLogout}) => {
+const MainLayout = ({isAuthed, onLogout, price}) => {
     return (
         <>
-            <Header isAuthed={isAuthed} onLogout={onLogout} />
+            <Header isAuthed={isAuthed} onLogout={onLogout} price={price}/>
             <Outlet />
         </>
     )
