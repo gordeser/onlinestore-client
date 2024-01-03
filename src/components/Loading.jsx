54d20036921d6cb9ddb89cca54fa42a4256a4f9f
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-03 14:28:53 +0100

    add Loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+    return (
+        <div className="container">
+            <h1 className="mb-3 text-center fw-normal">Loading...</h1>
+        </div>
+    )
+}
+
+export default Loading;
