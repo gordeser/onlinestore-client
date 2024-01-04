da0b6f041f056e2e2978879a31f818bc8005a193
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-04 11:19:35 +0100

    create Orders component

---
+++ src/components/Orders.jsx
@@ -0,0 +1,9 @@
+const Orders = () => {
+    return (
+        <div className="container">
+            <h1 className="mb-5 text-center fw-normal">Orders</h1>
+        </div>
+    )
+}
+
+export default Orders;
