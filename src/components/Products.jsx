5244bc53555b0a68035c78b2f387890b0d32f85b
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-04 12:00:54 +0100

    reconstruct loading on Products page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -37,15 +37,6 @@ const Products = ({ onChange }) => {
         onChange(productPrice);
     }
 
-    if (products.length === 0) {
-        return (
-            <div className="container">
-                <h1 className="mb-3 text-center fw-normal">Products</h1>
-                <Loading />
-            </div>
-        )
-    }
-
     return (
         <div className="container">
             <h1 className="mb-3 text-center fw-normal">Products</h1>
@@ -60,24 +51,26 @@ const Products = ({ onChange }) => {
                     </select>
                 </div>
             </div>
-
-            <div className="row row-cols-2 row-cols-md-5 g-4">
-                {products.map(product => (
-                    <div key={product.id} className="col">
-                        <div className="card h-100 shadow" style={{width: '100%'}}>
-                            <img src={product.image} className="card-img-top p-2" alt={product.name} />
-                            <br />
-                            <div className="card-body">
-                                <Link to={`/products/${product.id}`}>
-                                    <h5 className="card-title mb-4">{product.name}</h5>
-                                </Link>
-                                <p className="card-text"><strong>Price: </strong>{product.price}</p>
-                                <button className="btn btn-primary" onClick={() => handleAdd(product.id, product.price)}>Add to cart</button>
+            {products.length === 0 ? (<Loading />) : (
+                <div className="row row-cols-2 row-cols-md-5 g-4">
+                    {products.map(product => (
+                        <div key={product.id} className="col">
+                            <div className="card h-100 shadow" style={{width: '100%'}}>
+                                <img src={product.image} className="card-img-top p-2" alt={product.name} />
+                                <br />
+                                <div className="card-body">
+                                    <Link to={`/products/${product.id}`}>
+                                        <h5 className="card-title mb-4">{product.name}</h5>
+                                    </Link>
+                                    <p className="card-text"><strong>Price: </strong>{product.price}</p>
+                                    <button className="btn btn-primary" onClick={() => handleAdd(product.id, product.price)}>Add to cart</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            )}
+
         </div>
     );
 };
