42230e8d02c9b7208988eeddb0394bdb2da53ce4
Sergei Gordeev <gordeser@fit.cvut.cz>
2024-01-05 20:42:30 +0100

    fix menu url

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,7 +4,7 @@ const Menu = ({price}) => {
     return (
         <nav className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
             <NavLink to="." className="nav-link px-2">Home</NavLink>
-            <NavLink to="products" className="nav-link px-2">Products</NavLink>
+            <NavLink to="product" className="nav-link px-2">Products</NavLink>
             <NavLink to="cart" className="nav-link px-2">Cart: {price} CZK</NavLink>
         </nav>
     )
